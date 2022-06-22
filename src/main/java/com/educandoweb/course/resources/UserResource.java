ac50a1ccda5ce231e8fe9b395f5688071e630c57
Victor Muniz <kingkairuus@gmail.com>
2022-06-22 16:34:30 -0300

    H2 database, test profile, JPA

--- src/main/java/com/educandoweb/course/resources/UserResource.java
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -5,15 +5,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.educandoweb.course.entities.User;
+import com.educandoweb.course.entities.Person;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 	
 	@GetMapping
-	public ResponseEntity<User> findAll(){
-		User u = new User(1L,"Kairuus","kingkairuus@gmail.com","929939232","2323232");
+	public ResponseEntity<Person> findAll(){
+		Person u = new Person(1L,"Kairuus","kingkairuus@gmail.com","929939232","2323232");
 		return ResponseEntity.ok().body(u);
 	}
 
