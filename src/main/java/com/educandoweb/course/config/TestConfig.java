c0c51eba24e2684d931c666f1216da8fd730017d
Victor Muniz <kingkairuus@gmail.com>
2022-06-27 12:29:43 -0300

    Product

--- src/main/java/com/educandoweb/course/config/TestConfig.java
+++ src/main/java/com/educandoweb/course/config/TestConfig.java
@@ -11,15 +11,20 @@ import org.springframework.context.annotation.Profile;
 import com.educandoweb.course.entities.Category;
 import com.educandoweb.course.entities.Order;
 import com.educandoweb.course.entities.Person;
+import com.educandoweb.course.entities.Product;
 import com.educandoweb.course.entities.enums.OrderStatus;
 import com.educandoweb.course.repositories.CategoryRepository;
 import com.educandoweb.course.repositories.OrderRepository;
+import com.educandoweb.course.repositories.ProductRepository;
 import com.educandoweb.course.repositories.UserRepository;
 
 @Configuration
 @Profile("test")
 public class TestConfig implements CommandLineRunner{
 
+	@Autowired
+	private ProductRepository productRepository;
+	
 	@Autowired
 	private CategoryRepository categoryRepository;
 	
@@ -37,6 +42,14 @@ public class TestConfig implements CommandLineRunner{
 		Category cat3 = new Category(null, "Computers");
 		
 		categoryRepository.saveAll(Arrays.asList(cat1,cat2,cat3));
+		
+		Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "");
+		Product p2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "");
+		Product p3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
+		Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
+		Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, ""); 
+		
+		productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
 	
 		Person u1 = new Person(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		Person u2 = new Person(null, "Alex Green", "alex@gmail.com", "977777777", "123456"); 
