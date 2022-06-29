d6c2cd9dbf08168a5d276d14f82fd776acab54f1
Victor Muniz <kingkairuus@gmail.com>
2022-06-29 13:30:39 -0300

    User update

--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -31,4 +31,16 @@ public class UserService {
 	public void delete(Long id) {
 		repository.deleteById(id);
 	}
+	
+	public Person update(Long id, Person obj) {
+		Person entity = repository.getReferenceById(id);
+		updateData(entity,obj);
+		return repository.save(entity);
+	}
+
+	private void updateData(Person entity, Person obj) {
+		entity.setName(obj.getName());	
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());
+	}
 }
